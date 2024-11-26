b894e3b582c89dbc8f0d7d613305bc0eb99afeeb
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-11-26 21:26:44 +0530

    setup fonts and themes

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-4xl font-bold text-emerald-500">Hello, World!</h1>
+      <h1 className="text-4xl font-bold text-violet-300">Hello, World!</h1>
     </main>
   );
 };
