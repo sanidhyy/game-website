dfe1dc7e874b44a67299daecf7c4c7245ff81098
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-11-29 19:37:16 +0530

    about page implemented

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import { Hero } from "@/components/hero";
+import { About } from "@/components/about";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   );
 };
